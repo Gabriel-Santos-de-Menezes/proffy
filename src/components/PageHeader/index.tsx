a2ca4378cd907562ad176e0a7d1d7f3f2ee5ccc1
Gabriel Menezes <gabriel.santos.m112@gmail.com>
2020-08-03 23:13:09 -0300

    Creating global css and landing page

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
+
+//definir a tipagem de um objeto
+interface PageHeaderProps{
+    title: string;
+}
+
+//componente escrito em forma de função
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+    
+    return (
+        <header className="page-header">
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar"/>
+                </Link>
+                <img src={logoImg} alt=""/>
+            </div>
+
+
+            <div className="header-content">
+                <strong>{props.title}</strong>
+                {props.children}
+            </div>
+        </header>
+    );
+}
+
+export default PageHeader;
